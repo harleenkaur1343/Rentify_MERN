e1e1a2766874c9f646363daf398d94de7489baa9
Harleen Kaur <harleenkaur1343@gmail.com>
2024-05-27 20:27:40 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,23 @@
+# Home Rental Website
+
+This project is a home rental website built with React, Express, and MongoDB. The application allows users to register as either buyers or sellers. Sellers can list their properties, and users can view seller profiles and properties.
+
+## Features
+
+- User Registration (Buyer/Seller)
+- Seller Profile
+- List of Properties
+
+## Technologies Used
+
+- React
+- React Router
+- Express
+- MongoDB
+- Mongoose
+- JavaScript
+
+### Prerequisites
+
+- Node.js
+- MongoDB
---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+
+const app = express();
+
+// to display a static file on the server
+
+//app.use(express.static("static"));
+
+const userRoutes = require("./backend/routes/user");
+const propertyRoutes = require("./backend/routes/property");
+const db = require("./backend/config/db");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+//middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+mongoose
+  .connect(db)
+  .then(() => console.log("Database connected!"))
+  .catch((err) => console.error(err));
+
+app.use("/user", userRoutes);
+app.use("/property", propertyRoutes);
+
+app.listen(5000, function () {
+  console.log("the server is running on the port 5000");
+});
