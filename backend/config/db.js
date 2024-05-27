76d6bdbb9fa860e4b509346455f57230b0ba3789
Harleen Kaur <harleenkaur1343@gmail.com>
2024-05-27 20:29:06 +0530

    Add files via upload

---
+++ backend/config/db.js
@@ -0,0 +1,5 @@
+const mongoose = require('mongoose');
+
+const db = 'mongodb://127.0.0.1:27017/rentify';
+
+module.exports=db;
---
+++ backend/routes/user.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const {
+  signupUser,
+  allUsers,
+  oneSeller
+} = require("../controllers/userController");
+const router = express.Router();
+
+
+router.post("/signup", signupUser);
+router.get('/allusers',allUsers);
+router.get('/seller/:sellerId',oneSeller)
+
+module.exports = router;
