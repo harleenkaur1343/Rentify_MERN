8f934932d56c08bbff53c1ec0fb1537962749409
Harleen Kaur <harleenkaur1343@gmail.com>
2024-06-03 13:45:32 +0530

    Delete backend directory

--- backend/config/db.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require("mongoose");
-
-const db =
-  "mongodb+srv://harleenkaur1343:YK91MA0OE73AztYO@cluster0.qkkf8jf.mongodb.net/rentify?retryWrites=true&w=majority&appName=Cluster0";
-
-module.exports = db;
